0970cca84d910b41a3c8cae9c1bda4ff7e7280c9
PeymanBozorgnezhad <bozorgnezhadpeyman@gmail.com>
2022-03-25 19:15:22 +0430

    color added to inputDecoration

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,5 @@ import 'package:flutter/material.dart';
 
 // Color kBackGroundScaffold =  const Color.fromARGB(243,244,253,255);
 Color kBackColorScaffold = const Color(0xff030317);
+Color kColorInputDecoration = const Color(0xFFc41a43);
+
